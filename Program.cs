262140a6cb47817403bcb58e521aa7d58d7f08f1
ahmedhh33 <ahmedhh3633@gmail.com>
2023-06-01 11:48:09 +0400

    patter

--- Program.cs
+++ Program.cs
@@ -78,6 +78,16 @@
             Console.WriteLine("  *");
             Console.WriteLine(" *");
             Console.WriteLine("*");
+
+            for (int i = 1; i <=5; i++)
+            {
+                for (int j = 1; j <=i; j++)
+                {
+                    Console.Write(j);
+                }
+                
+                Console.WriteLine("");
+            }
         }
     }
 }
